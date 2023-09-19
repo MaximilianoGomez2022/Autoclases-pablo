d5529a276e5d6b0846b41d605201e3cbfb7516d7
maxi <maxigomez.udaondo@gmail.com>
2023-09-19 10:12:28 -0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -28,7 +28,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+  margin: 39px 0 0;
 }
 ul {
   list-style-type: none;
